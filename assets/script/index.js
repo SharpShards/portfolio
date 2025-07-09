24a630f152f95330e4e3b248465735def29e0c4a
Lucas Rafael <lucas15azevedo@gmail.com>
2025-07-08 23:56:16 -0300

    [update] Hero Area

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,25 @@
+// Funções
+function mudarImagem(){
+    let imagens = ["starbucks", "orangePoints"];
+
+    // Verifica qual imagem deve ir
+    // Quando chegar na última, zera o contador e volta pra primeira.
+    if(animaHeroImage == (imagens.length - 1)){
+        animaHeroImage = 0      
+    }else{
+        animaHeroImage += 1
+    }
+    
+    heroImage.setAttribute("src", `assets/images/mockups/${imagens[animaHeroImage]}.svg`);
+}
+
+function moverPortfolio(){
+    sectionProjetos.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
+
 function expandirSkill(){
     let sk = this.children[1];
     let seta = this.children[0].children[0];
@@ -30,17 +52,22 @@ function normalizarBotao(){
     this.children[0].style.fontWeight = "600";
 }
 
+// Declarações
+let animaHeroImage = 0; 
+
+const heroImage = document.querySelector("header #dvHeroArea #dvHAImagens img");
+    setInterval(mudarImagem, 4000);
+const herobutton = document.querySelector("header #dvHeroArea #dvHATexto button");
+    herobutton.addEventListener("click", moverPortfolio);
 const cards = document.querySelectorAll('#sctSkills .dvSeg .dvCard');
-for(let loop = 0; loop < cards.length; loop++){
-    cards[loop].addEventListener('click', expandirSkill);
-}
-
+    for(let loop = 0; loop < cards.length; loop++){
+        cards[loop].addEventListener('click', expandirSkill);
+    }
 const button = document.querySelectorAll("#sctProjetos .dvPri .dvSub button");
-for(let loop = 0; loop < button.length; loop++){
-    button[loop].addEventListener('mouseover', brilharBotao);
-    button[loop].addEventListener('mouseout', normalizarBotao);
-}
-
+    for(let loop = 0; loop < button.length; loop++){
+        button[loop].addEventListener('mouseover', brilharBotao);
+        button[loop].addEventListener('mouseout', normalizarBotao);
+    }
 const a = document.querySelectorAll('#sctProjetos .dvPri .dvSub button a');
-
 const seta = document.querySelectorAll('.dvSeta');
+const sectionProjetos = document.querySelector("#sctProjetos");
