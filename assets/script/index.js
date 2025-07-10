d29b0dbc04225241679b9bd01a1c3bc4fab4f821
Lucas Rafael <lucas15azevedo@gmail.com>
2025-07-10 20:01:21 -0300

    [feat] Alternar entre formações

--- assets/script/index.js
+++ assets/script/index.js
@@ -20,6 +20,51 @@ function moverPortfolio(){
     });
 }
 
+function ativarBolha(){
+    // Desativa bolha anterior
+    let anterior = document.getElementsByClassName("dvAtivada");
+    
+    anterior[0].removeAttribute("class");
+
+    // Ativa bolha atual
+    this.setAttribute("class", "dvAtivada");
+
+    // Muda texto
+    let elemento = this.getAttribute("id");
+
+    if(elemento == "dvPri"){
+        formacao.children[0].innerHTML = "Operador de Computador";
+        formacao.children[1].innerHTML = "Prepara";
+        formacao.children[2].innerHTML = "Curso";
+        formacao.children[3].innerHTML = "2017";
+    }else if(elemento == "dvSeg"){
+        formacao.children[0].innerHTML = "Game Design";
+        formacao.children[1].innerHTML = "Zion";
+        formacao.children[2].innerHTML = "Curso";
+        formacao.children[3].innerHTML = "2018 - 2020";
+    }else if(elemento == "dvTer"){
+        formacao.children[0].innerHTML = "Ensino Médio";
+        formacao.children[1].innerHTML = "CIOB";
+        formacao.children[2].innerHTML = "Formação";
+        formacao.children[3].innerHTML = "2020 - 2022";
+    }else if(elemento == "dvQua"){
+        formacao.children[0].innerHTML = "Programação Full-Stack";
+        formacao.children[1].innerHTML = "B7Web";
+        formacao.children[2].innerHTML = "Curso";
+        formacao.children[3].innerHTML = "Cursando";
+    }else if(elemento == "dvQui"){
+        formacao.children[0].innerHTML = "Segurança Cibernética";
+        formacao.children[1].innerHTML = "INMETRO";
+        formacao.children[2].innerHTML = "Técnico";
+        formacao.children[3].innerHTML = "2023 - 2024";
+    }else if(elemento == "dvSex"){
+        formacao.children[0].innerHTML = "Engenharia Mecânica";
+        formacao.children[1].innerHTML = "UCP";
+        formacao.children[2].innerHTML = "Graduação";
+        formacao.children[3].innerHTML = "Cursando";
+    }
+}
+
 function expandirSkill(){
     let sk = this.children[1];
     let seta = this.children[0].children[0];
@@ -55,14 +100,22 @@ function normalizarBotao(){
 // Declarações
 let animaHeroImage = 0; 
 
+// Elementos
 const heroImage = document.querySelector("header #dvHeroArea #dvHAImagens img");
-    setInterval(mudarImagem, 4000);
 const herobutton = document.querySelector("header #dvHeroArea #dvHATexto button");
     herobutton.addEventListener("click", moverPortfolio);
+
 const cards = document.querySelectorAll('#sctSkills .dvSeg .dvCard');
     for(let loop = 0; loop < cards.length; loop++){
         cards[loop].addEventListener('click', expandirSkill);
     }
+const bolhas = document.querySelectorAll("#sctQuemSou .dvInfos #dvTrajetoria #dvBolhas div");
+    for(let loop = 0; loop < bolhas.length; loop++){
+        bolhas[loop].addEventListener("click", ativarBolha);
+    }
+const formacao = document.querySelector("#sctQuemSou .dvInfos #dvTrajetoria #dvFormacao");
+
+const sectionProjetos = document.querySelector("#sctProjetos");
 const button = document.querySelectorAll("#sctProjetos .dvPri .dvSub button");
     for(let loop = 0; loop < button.length; loop++){
         button[loop].addEventListener('mouseover', brilharBotao);
@@ -70,4 +123,7 @@ const button = document.querySelectorAll("#sctProjetos .dvPri .dvSub button");
     }
 const a = document.querySelectorAll('#sctProjetos .dvPri .dvSub button a');
 const seta = document.querySelectorAll('.dvSeta');
-const sectionProjetos = document.querySelector("#sctProjetos");
+
+// Main
+//// Animação Hero Image
+setInterval(mudarImagem, 4000);
